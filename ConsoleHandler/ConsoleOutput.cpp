70804a9b97144f06098366378054b2fc8fb90264
Ruffo <groothoff@united-mail.org>
2017-12-05 10:55:16 +0100

    Update ConsoleOutput.cpp
    MenuDisplayTest

--- ConsoleHandler/ConsoleOutput.cpp
+++ ConsoleHandler/ConsoleOutput.cpp
@@ -34,4 +34,6 @@ namespace console_handler
     }
     print(console_type, "\n" + print_str);
   }
+  
+   /**  * \brief Displays a menu structure & returns the menu index  */    int ConsoleOutput::dipslay_menu(std::string menu_title, std::string[] menu_entrys)  {    // Write menu    print_line(menu, "-");    POINT cursor_position;    std::GetCursorPos(&cursor_position);    int menu_item_poss[menu_entrys.length];     for(int i = 0; i < menu_entrys.length; i++)    {      std::GetCursorPos(&cursor_position);      menu_item_poss[i] = cursor_position.y;      print(menu, i.c_str() + " - " +  menu_entrys[i].c_str());    }        print(menu, "Please select one entry. (arrow keys)");    print_line(menu, "-");        // Handle menu selection    int keyInput;    int currentItemIndex = menu_entrys.length;    std::SetCursorPos(0, menu_item_poss[currentItemIndex]);    while(keyInput != 13)    {      keyInput = getch();      switch(keyInput)      {        case KEY_UP:          currentItemIndex--;          if(currentItemIndex < 0)            currentItemIndex = menu_entrys.length;          break;        case KEY_DOWN:          currentItemIndex++;          if(currentItemIndex > menu_entrys.length)            currentItemIndex = 0;          break;      }      std::SetCursorPos(0, menu_item_poss[currentItemIndex]);    }        print(menu, "Selected: " + menu_entrys[currentItemIndex]);  }
 }
